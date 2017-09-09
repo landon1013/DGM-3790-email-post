2f16f768ca987c4895ab79434b5524c051d4b7e1
Krista Bice <bubbybice@hotmail.com>
2017-09-09 13:01:14 -0600

    just fixing some things i had changed and deleting unecissary code.

--- email-php.php
+++ email-php.php
@@ -3,12 +3,10 @@
   $email = $_POST['form_email'];
   $message = $_POST['form_msg'];
 
-  $to = "bubgirl17@gmail.com";
+  $to = "landon@thecallfamily.com";
   $subject = "RIA Emails";
   $body = "Name: ".$name."\nEmail: ".$email."\nMessage: ".$message;
   $headers = "From: " . $email;
-
-	echo '<script> alert("your message has been successfully sent") </script>';
 
   //send email
   mail($to, $subject, $body, $headers);
